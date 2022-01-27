5d8a43094b7b0daf5510b7887ae68d79b2265f51
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-27 17:02:07 +0100

    Jnasck 3 Completed

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,19 +1,20 @@
 // Creao un array con dei numeri
 let numbers = ["10","2","1","3","5","7","11","20","12","6","0","19","13","4","8"];
+
 // Seleziono solo i primi 10 numeri di quell'array
 let selects = (numbers.slice(0, 10));
-console.log(selects[5])
+
 // Calcolo la somma tra i numeri selezionati
-let somma;
+let somma = 0;
 for (let i = 0; i < selects.length; i++){
-  let numberSelect = parseInt(numbers[i]);
-  somma += numbers[i];
-  console.log(numberSelect);
-  console.log(typeof numberSelect);
+  let numberSelect = parseInt(selects[i]);
+  somma += numberSelect;
 }
-
-console.log(somma)
-
-
-
 // Calcolo una media tra i numeri selezioni
+let media = somma / selects.length;
+
+alert("La somma dei primi 10 numeri è: " + somma + ", mentre la media è: " +  media);
+
+
+
+
